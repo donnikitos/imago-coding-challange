3b1f7c8933a85b7b19045589f8c8d616a375f5ea
Nikita Nitichevski <nikita.n@nititech.de>
2026-01-28 23:14:27 +0100

    wait for client components

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,15 +1,18 @@
 import { Filters } from '@/app/(pages)/_components/Filters';
 import { PaginationWrapper } from '@/app/(pages)/_components/PaginationWrapper';
 import { ImageGrid } from '@/app/(pages)/_components/ImageGrid';
+import { Suspense } from 'react';
 
 export default function Home() {
 	return (
 		<main className="flex-1 flex flex-col max-w-7xl w-full mx-auto">
-			<Filters />
+			<Suspense>
+				<Filters />
 
-			<ImageGrid />
+				<ImageGrid />
 
-			<PaginationWrapper />
+				<PaginationWrapper />
+			</Suspense>
 		</main>
 	);
 }
